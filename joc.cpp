3ba2c4b46e270cd5c9d4ac9b2563093b6b1b1c17
JoanRosell <vorian97@gmail.com>
2019-04-03 01:45:37 +0200

    add Session 1 functionalities

--- joc.cpp
+++ joc.cpp
@@ -15,22 +15,32 @@ void joc()
 	// AFEGIR CODI: Crear tots els grafics del joc
 	//				Inicialitzar variables posicio vaixell
 	// ********************************************
+	Sprite ship("C:\\dev\\BatallaNaval\\Program\\data\\vaixell.png");
+	Sprite board("C:\\dev\\BatallaNaval\\Program\\data\\caselles.png");
+	int grid_X = 0;
+	int grid_Y = 0;
 	do 
 	{
 		// Captura tots els events de ratolí i teclat de l'ultim cicle
 		pantalla.processEvents();
-			
+		
+		
 		// *****************************************************************************
 		// AFEGIR CODI:	Capturar si s'ha fet clic amb el ratolí
 		//				Si s'ha fet clic amb el ratolí modificar posició (fila i columna del tauler) 
 		//				del vaixell en funció de la posició del ratolí
 		// *****************************************************************************
-		
+		if (Mouse_getButLeft())
+		{
+			grid_X = ((int) Mouse_getX() / MIDA_CASELLA) * MIDA_CASELLA;
+			grid_Y = ((int) Mouse_getY() / MIDA_CASELLA) * MIDA_CASELLA;
+		}
 		// *****************************************************************************
 		// AFEGIR CODI: Redibuixar tauler 
 		//				Dibuixar el vaixell a la posició (fila i columna) que toqui						
 		// *****************************************************************************
-
+		board.draw(INI_PANTALLA_X, INI_PANTALLA_Y);
+		ship.draw(grid_X, grid_Y);
 		// Actualitza la pantalla
 		pantalla.update();
 
